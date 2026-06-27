8a5c1ce6bba5b4f84182eb5a9bdc776d71a9227b
Yama Sadid <rayman_jan@hotmail.com>
2026-06-27 17:16:34 +0200

    added calculator file

---
+++ Calculator/src/calculator.java
@@ -0,0 +1,14 @@
+
+public class Calcculator{
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    public static int subract(int a, int b){
+        return a-b;
+    }
+
+    public static void main(String[] args){
+        System.out.println(add(5,3));
+    }
+}
